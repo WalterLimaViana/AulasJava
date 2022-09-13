79478896b4db599a5bbf06250da9494fb0308c3b
WalterLimaViana <walter.lima.viana@gmail.com>
2022-09-13 20:39:59 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+//        String nome = "Walter";
+//        System.out.println("Hello " + nome);
+//        boolean fimDeSemana = true;
+//        boolean fazendoSol = true;
+
+//        int nota = 55;
+//        String graduacao;
+//
+//        if(nota >= 80) {
+//            graduacao = "A";
+//            System.out.println("Média A");
+//        } else if (nota < 80 && nota >=70) {
+//            graduacao = "B";
+//            System.out.println("Média B");
+//        } else if (nota < 70 && nota >=60) {
+//            System.out.println("Média C");
+//            graduacao = "C";}
+//        else if (nota < 60 && nota >=0) {
+//            System.out.println("Média D");
+//            graduacao = "D";
+//        } else {
+//            graduacao ="";
+//        }
+//
+//        switch (graduacao){
+//            case "A":
+//            case "B":
+//                System.out.println("Aluno Aprovado");
+//                break;
+//            case "C":
+//            case "D":
+//                System.out.println("Aluno Reprovado");
+//                break;
+//            default:
+//                System.out.println("Nota inválida");
+//        }
+
+
+    }
+}
